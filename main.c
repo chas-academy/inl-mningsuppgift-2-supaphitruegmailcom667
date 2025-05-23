0783a2d41e0685932f9dd28fcc2d5b6fe289454b
supaphit.rue@gmail.com <supaphit.rue@gmail.com>
2025-05-23 13:08:43 +0200

    first try

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+float fahrenheit_to_celsius(float f){
+    float c = (f-32)*5.0/9.0;
+    return c;
+}
 
-// This is where you add the two functions
+float celsius_to_fahrenheit(float c){
+    float f = (c*9.0)/5.0 + 32;
+    return f;
+}
+
 
 int main() {
     float temp_f = 100.0;
